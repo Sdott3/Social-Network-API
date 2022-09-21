bf14d5b78875e666e20c514115e33e7ba904c125
sdott3 <sdotthomas347@gmail.com>
2022-09-20 22:48:55 -0400

    finished user schema

---
+++ models/User.js
@@ -0,0 +1,43 @@
+// Import dependencies
+const { Schema, model } = require('mongoose');
+
+// Username Schema
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true, 
+        // REGEX to validate correct email
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    },
+    {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+})
+
+const Users = model('Users', userSchema);
+
+
+module.exports = Users;
