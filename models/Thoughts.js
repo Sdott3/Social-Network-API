498585aebc82579d82a82cd2351fa282d14af8b8
sdott3 <sdotthomas347@gmail.com>
2022-09-21 14:45:24 -0400

    added thought schema to thought.js

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,1 +1,41 @@
 // Import dependencies
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ThoughtsSchema = new Schema ({
+    thoughtText:{
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal) 
+    },
+    username:{
+        type: String,
+        required: true  
+    },
+    reaction: [ReactionsSchema]
+    },
+    {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+
+
+
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reaction.length
+})
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
